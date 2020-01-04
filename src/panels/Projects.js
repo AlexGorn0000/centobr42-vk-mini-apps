86e0fb331715a2a7ee91a5864a07b3e4a9098c7b
AlexGorn0000 <49290929+AlexGorn0000@users.noreply.github.com>
2020-01-04 22:59:54 +0300

    Add files via upload

--- src/panels/Projects.js
+++ src/panels/Projects.js
@@ -11,6 +11,7 @@ import InfoRow from '@vkontakte/vkui/dist/components/InfoRow/InfoRow';
 import Progress from '@vkontakte/vkui/dist/components/Progress/Progress';
 import List from '@vkontakte/vkui/dist/components/List/List';
 import { FormLayout, FormLayoutGroup, Input, FormStatus, Search, CellButton } from '@vkontakte/vkui';
+import Placeholder from '@vkontakte/vkui/dist/components/Placeholder/Placeholder';
 import { HeaderButton } from '@vkontakte/vkui';
 ///Icons
 import Icon24Back from '@vkontakte/icons/dist/24/back';
@@ -25,13 +26,12 @@ import Icon24MoneyTransfer from '@vkontakte/icons/dist/24/money_transfer';
 import Icon24BrowserBack from '@vkontakte/icons/dist/24/browser_back';
 import Icon24Market from '@vkontakte/icons/dist/24/market';
 import Icon24LogoVk from '@vkontakte/icons/dist/24/logo_vk';
-import Icon56FavoriteOutline from '@vkontakte/icons/dist/56/favorite_outline';
-import Icon56FireOutline from '@vkontakte/icons/dist/56/fire_outline';
 import Icon24Help from '@vkontakte/icons/dist/24/help';
 import Icon24Add from '@vkontakte/icons/dist/24/add';
 import Icon24Services from '@vkontakte/icons/dist/24/services';
 import Icon24Note from '@vkontakte/icons/dist/24/note';
 import Icon24Live from '@vkontakte/icons/dist/24/live';
+import Icon56NotificationOutline from '@vkontakte/icons/dist/56/notification_outline';
 import Icon24Gift from '@vkontakte/icons/dist/24/gift';
 import Icon24PlayNext from '@vkontakte/icons/dist/24/play_next';
 import Icon24Bug from '@vkontakte/icons/dist/24/bug';
@@ -51,7 +51,7 @@ const Profile = ({ id, go, fetchedUser }) => (
      <Cell expandable before={<Avatar src="https://sun9-18.userapi.com/c845521/v845521060/185e69/-Poj5OWvw0I.jpg"/>}>?????? ?? ???????????</Cell>
      <Cell expandable before={<Avatar src="https://sun9-56.userapi.com/c853524/v853524614/180a74/EmbF7mDRjCM.jpg"/>}>??????? "? ?? ??? ????? ????? ??????!"</Cell>
      <Cell expandable before={<Avatar src="https://sun9-49.userapi.com/c858224/v858224433/ecc30/KFgggeOEr9w.jpg"/>}>???????? ?????-?????? ??? ?.???????</Cell>
-     <Placeholder icon={<Icon56FireOutline/>} title="?????? ?????? ?????????" action={<Button size="l" href="https://vk.com/centobr42_press_center">?????????</Button>}>???????? ? ?????? ?????-?????? ? ????? ?? ????? ?????? ? ????? ????????!</Placeholder>
+     <Placeholder icon={<Icon56NotificationOutline/>} title="?????? ?????? ?????????" action={<Button size="l" href="https://vk.com/centobr42_press_center">?????????</Button>}>???????? ? ?????? ?????-?????? ? ????? ?? ????? ?????? ? ????? ????????!</Placeholder>
     </Group>
  </Panel>
 );
