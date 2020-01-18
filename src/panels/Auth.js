8dcd936355d12c56db445b618150593a03a5e1e0
AlexGorn0000 <49290929+AlexGorn0000@users.noreply.github.com>
2020-01-18 17:31:17 +0300

    Add files via upload

--- src/panels/Auth.js
+++ src/panels/Auth.js
@@ -7,7 +7,7 @@ import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import List from '@vkontakte/vkui/dist/components/List/List';
-import { FormLayout, FormLayoutGroup, Input, FormStatus, HorizontalScroll, Separator} from '@vkontakte/vkui';
+import { FormLayout, FormLayoutGroup, Input, FormStatus, HorizontalScroll, Separator, Select} from '@vkontakte/vkui';
 import { HeaderButton } from '@vkontakte/vkui';
 ///Icons
 import Icon24Back from '@vkontakte/icons/dist/24/back';
@@ -21,6 +21,7 @@ import Icon28ArticleOutline from '@vkontakte/icons/dist/28/article_outline';
 import Icon28Mention from '@vkontakte/icons/dist/28/mention';
 import Icon28RecentOutline from '@vkontakte/icons/dist/28/recent_outline';
 import Icon56EventOutline from '@vkontakte/icons/dist/56/event_outline';
+import Icon12Verified from '@vkontakte/icons/dist/12/verified';
 import '../css/style.css';
 /*
 const users = [
@@ -48,26 +49,34 @@ const users = [
   ];
 */
 const Auth = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader left={<HeaderButton onClick={go} Data-to="Started"><Icon24BrowserBack/></HeaderButton>}>Вход в учетную запись</PanelHeader>
-		<Group title="Что нового?">
-		<Div style={{ marginLeft: '10px 0' }} className="animation_1" before={<Icon56EventOutline/>}></Div>
-		<Separator style={{ margin: '10px 0' }} />
-		<Cell before={<Icon28RecentOutline/>}>Будьте всегда в центре событий!</Cell>
-		<Cell before={<Icon28ArticleOutline/>}>Следите за расписанием уроков в разделе "Образование"</Cell>
-		<Cell before={<Icon28Mention/>}>Получайте сведения о мероприятиях!</Cell>
-		</Group>
+	<Panel id={id} theme="client_dark">
+		<div className="background_auth"/>
+		<PanelHeader>Вход в учетную запись</PanelHeader>
 		<Group>
-		<Div>Чтобы авторизировать свою учетную запись, Вы должны заполнить данные в окно ввода</Div>
+		<Cell style={{marginLeft: '145px'}} before={<Icon56EventOutline/>}></Cell>
+		<h4 style={{textAlign: 'center'}}>Добро пожаловать в<br/>Личный кабинет</h4>
+		<Separator style={{margin: '5px 0'}}/>
+		<Cell before={<Icon28RecentOutline/>}>Будьте всегда в центре событий!</Cell>
+		<Cell before={<Icon28ArticleOutline/>}>Будьте в курсе завтрашних уроков!</Cell>
+		<Cell before={<Icon28Mention/>}>Получайте сведения о мероприятиях!</Cell>
+		<Separator style={{margin: '5px 0'}}/>
+		<FormLayout>
+		<FormLayoutGroup>
+		<h4 style={{marginLeft: '15px'}}>Выберите платформу</h4>
+		<Select>
+		<option value="gosuslugi">ГосУслуги</option>
+		<option value="ВКонтакте">ВКонтакте</option>
+		</Select>
+		</FormLayoutGroup>
+		</FormLayout>
 		<FormLayout>
         <FormLayoutGroup>
+		<h4 style={{marginLeft: '15px'}}>Введите данные для входа</h4>
         <Input type="login" placeholder="Введите логин" />
 		<Input type="password" placeholder="Введите пароль"/>
         </FormLayoutGroup>
         </FormLayout>
-		<Button size="xl" level="primary" onClick={go} Data-to="Home">
-		  Войти
-		</Button>
+		<Button size="xl" level="primary" onClick={go} Data-to="Home">Войти</Button>
 		</Group>
 	</Panel>	
 );
