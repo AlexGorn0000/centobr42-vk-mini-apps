f13c1859460b7f86dfc6c1d61e7dd8a3fd11cf36
AlexGorn0000 <49290929+AlexGorn0000@users.noreply.github.com>
2020-01-19 00:20:00 +0300

    Add files via upload

--- src/panels/Auth.js
+++ src/panels/Auth.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes, { string, any } from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
@@ -106,10 +106,10 @@ const users = [
 		</Select>
 		<br></br>
 		<h4 style={{marginLeft: '20px'}}>Введите данные для входа:</h4>
-        <Input type="login" name="login" value={login} onChange={this.onChange} status={login ? 'valid' : 'error' } bottom={login ? 'Ваш логин успешно инициализирован' : 'Ошибка: 0x52d270. Ваши данные не внесены в систему!' } placeholder="Введите логин" />
-		<Input type="password" name="password" value={password} onChange={this.onChange} status={password ? 'valid' : 'error' } bottom={password ? 'Ваш пароль успешно инициализорован!' : 'Ошибка: 0x74d270. Ваши данные не внесены в систему!' } placeholder="Введите пароль"/>
+        <Input type="login" name="login" value={login} onChange={this.onChange} status={login? 'valid' : 'error' } bottom={login? 'Ваш логин успешно инициализирован' : 'Ошибка: 0x52d270. Ваши данные не внесены в систему!' } placeholder="Введите логин" />
+		<Input type="password" name="password" value={password} onChange={this.onChange} status={password? 'valid' : 'error' } bottom={password? 'Ваш пароль успешно инициализорован!' : 'Ошибка: 0x74d270. Ваши данные не внесены в систему!' } placeholder="Введите пароль"/>
 		<br></br>
-		<Button size="xl" level="primary" onChange={this.onChange} status={login, password ? 'valid' : 'error' } bottom={login, password ? 'Вы успешно авторизировались' : 'Ошибка: 0x76d270. Ваши данные не внесены в систему!' } onClick={this.props.go} Data-to="Home">Войти</Button>
+		<Button size="xl" level="primary" onChange={this.onChange} status={login, password? 'valid' : 'error' } bottom={login, password? 'Вы успешно авторизировались' : 'Ошибка: 0x76d270. Ваши данные не внесены в систему!' } onClick={this.props.go} Data-to="Home">Войти</Button>
 		</FormLayoutGroup>
         </FormLayout>
 		</Group>
