6e64caef75730ea025bf08d149610afaf459b8e5
AlexGorn0000 <49290929+AlexGorn0000@users.noreply.github.com>
2020-01-23 01:52:01 +0300

    Add files via upload

--- src/panels/classes/5/To5a.js
+++ src/panels/classes/5/To5a.js
@@ -83,7 +83,7 @@ const To5a = ({ id, go, fetchedUser}) => (
   5-ый урок: Родной язык<br/>
   6-ой урок: Литература</Div>
   <Separator style={{margin: '15px 0'}}/>
-  <Cell before={<Icon24ShareOutline/>} onClick="Likes.share('doc270919242_522292547', {}); return false;">Поделиться</Cell>
+  <Cell before={<Icon24ShareOutline/>} onClick="Likes.share('doc270919242_522292547', {}); return true;">Поделиться</Cell>
   </Group>
  </Panel>
 );
