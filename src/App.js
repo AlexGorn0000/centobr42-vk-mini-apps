8230f6d8350e8c1251ef9fa92aad888b068a526a
AlexGorn0000 <49290929+AlexGorn0000@users.noreply.github.com>
2020-01-30 04:19:43 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const App = () => {
 		
 		async function fetchData() {
 			const user = await connect.sendPromise('VKWebAppGetUserInfo');
-			connect.sendPromise("VKWebAppGetAuthToken", {"app_id": 7266393,"scope": 'notify,friends,photos'}); 
+			connect.sendPromise("VKWebAppGetAuthToken", {"app_id": 7266393,"scope": 'notify,friends,photos,poll'}); 
 			connect.sendPromise("VKWebAppJoinGroup", {"group_id": 168892763});
 			setUser(user);
 			setPopout(null);
---
+++ src/panels/general/Help.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import connect from '@vkontakte/vk-connect'; 
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import InfoRow from '@vkontakte/vkui/dist/components/InfoRow/InfoRow';
+import Progress from '@vkontakte/vkui/dist/components/Progress/Progress';
+import List from '@vkontakte/vkui/dist/components/List/List';
+import { FormLayout, FormLayoutGroup, Input, FormStatus, Search, CellButton, Separator, UsersStack } from '@vkontakte/vkui';
+import { HeaderButton } from '@vkontakte/vkui';
+///Icons
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+import Icon24Search from '@vkontakte/icons/dist/24/search';
+import Icon24Info from '@vkontakte/icons/dist/24/info';
+import Icon24Education from '@vkontakte/icons/dist/24/education';
+import Icon24UserOutgoing from '@vkontakte/icons/dist/24/user_outgoing';
+import Icon24User from '@vkontakte/icons/dist/24/user';
+import Icon24Newsfeed from '@vkontakte/icons/dist/24/newsfeed';
+import Icon24Settings from '@vkontakte/icons/dist/24/settings';
+import Icon24MoneyTransfer from '@vkontakte/icons/dist/24/money_transfer';
+import Icon24BrowserBack from '@vkontakte/icons/dist/24/browser_back';
+import Icon24Market from '@vkontakte/icons/dist/24/market';
+import Icon24LogoVk from '@vkontakte/icons/dist/24/logo_vk';
+import Icon24Help from '@vkontakte/icons/dist/24/help';
+import Icon24Add from '@vkontakte/icons/dist/24/add';
+import Icon24Services from '@vkontakte/icons/dist/24/services';
+import Icon24Note from '@vkontakte/icons/dist/24/note';
+import Icon24Live from '@vkontakte/icons/dist/24/live';
+import Icon24Gift from '@vkontakte/icons/dist/24/gift';
+import Icon24PlayNext from '@vkontakte/icons/dist/24/play_next';
+import Icon24Bug from '@vkontakte/icons/dist/24/bug';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import user from '@vkontakte/icons/dist/24/user';
+
+const help = ({ id, go, fetchedUser }) => (
+	<Panel id={id}>
+	<PanelHeader left={<HeaderButton onClick={go} Data-to="Home"><Icon24BrowserBack/></HeaderButton>}>??????</PanelHeader>
+ 	<Group>
+    <List>
+    <Cell>??? ??????, ???? ? ?? ???? ????? ? ???? ????????</Cell>
+    <Cell>????? ?? ?????????? ?????????? ????????????</Cell>
+    <Cell>??? ??????, ???? ? ???????? ????? ???????<br/>???????????? ?????????? ??????? ??? ? ??? ????? ?????? ?????????? ??????</Cell>
+    <Cell>? ?? ????? ?????? ??? ????? ?? ??????, ??? ????? ?????????? ? ????????????? ??????</Cell>
+    </List>
+    </Group>
+ </Panel>
+);
+
+Help.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	fetchedUser: PropTypes.shape({
+		photo_200: PropTypes.string,
+		first_name: PropTypes.string,
+		last_name: PropTypes.string,
+		city: PropTypes.shape({
+			title: PropTypes.string,
+		}),
+	}),
+};
+
+export default Help;
