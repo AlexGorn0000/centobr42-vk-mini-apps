7558575a2e78565861491390398195a65c176eda
AlexGorn0000 <49290929+AlexGorn0000@users.noreply.github.com>
2020-02-22 01:36:43 +0300

    Add files via upload

--- src/panels/education/classes/11/To11b.js
+++ src/panels/education/classes/11/To11b.js
@@ -91,7 +91,7 @@ const To11b = ({ id, go, fetchedUser}) => (
   6-ой урок: Литература<br/>
   7-ой урок: Обществознание</Div>
   <Separator style={{margin: '15px 0'}}/>
-  <Cell before={<Icon24ShareOutline/>} onClick={connect.send("VKWebAppShare", {"link": "https://vk.com/app7266393_-187421428"})}>Поделиться</Cell>
+  <Cell before={<Icon24ShareOutline/>} onClick={connect.subscribe("VKWebAppShare", {"link": "https://vk.com/app7266393_-187421428"})}>Поделиться</Cell>
   </Group>
  </Panel>
 );
